cbef7a819b082a16d8d8a2964fcfce38feb71152
kennethhowell <howell.kenneth.a@gmail.com>
2021-06-09 10:01:05 -0500

    initial comit of spring structure

---
+++ src/main/java/com/codeup/marcospringblog/MarcoSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.marcospringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcoSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarcoSpringBlogApplication.class, args);
+    }
+
+}
