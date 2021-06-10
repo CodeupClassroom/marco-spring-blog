3dbc8a7d997892c32c55e430126db7f0cd090038
Samuel Moore <samuel.c.moore1221@gmail.com>
2021-06-10 14:24:45 -0500

    Thank you for letting me lecture views

--- src/main/java/com/codeup/marcospringblog/HelloController.java
+++ src/main/java/com/codeup/marcospringblog/HelloController.java
@@ -1,12 +1,13 @@
 package com.codeup.marcospringblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
+public class  HelloController {
 
     @GetMapping("/hello")
     @ResponseBody
@@ -14,10 +15,19 @@ public class HelloController {
         return "Hello from Spring Boot to my friends in Marco!";
     }
 
+
+//     "Hello from Spring Boot to " + name + " in Marco!";
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String hello(@PathVariable String name){
-        return "Hello from Spring Boot to " + name + " in Marco!";
+    public String hello(
+            @PathVariable String name,
+            Model model
+    ){
+
+        String bio = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n";
+        model.addAttribute("bio",bio);
+        model.addAttribute("UsersName",name);
+
+        return "helloUser";
     }
 
 }
